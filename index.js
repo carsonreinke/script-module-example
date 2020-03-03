57e878393eef8d29b59be1ee339b6621049c447b
Carson Reinke <carson@reinke.co>
2020-03-02 21:34:12 -0500

    Added some comments

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//Create a global namespace like browsers have
 const Window = require('window');
 global.window = new Window();
 
+//Nothing is exported, since global namespace is modified
 //const mod = require('./script');
 require('./script');
 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Need to add function to global namespace, since nothing would be exported
 //function myFunc() {
 window.myFunc = function() {
     console.log('called');
